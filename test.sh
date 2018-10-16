0b88dc81607bf14de31e4a52e635c6cd13da3856
Nick Fitzgerald <fitzgen@gmail.com>
2018-10-16 05:39:30 -0700

    Run CI tests with larger static array

--- test.sh
+++ test.sh
@@ -16,6 +16,8 @@ time cargo test --release --features "extra_assertions"
 time cargo test --release --features "size_classes"
 time cargo test --release
 
+export WEE_ALLOC_STATIC_ARRAY_BACKEND_BYTES=$((1024 * 1024 * 1024))
+
 time cargo test --release --features "static_array_backend extra_assertions size_classes"
 time cargo test --release --features "static_array_backend extra_assertions"
 time cargo test --release --features "static_array_backend size_classes"
